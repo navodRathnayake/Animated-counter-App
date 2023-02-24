e3c15aee1ff067242c0bcf0a540099201ebd2075
Navod Rathnayake <82445185+navodRathnayake@users.noreply.github.com>
2023-02-24 07:54:53 +0530

    finalized the business logics

--- lib/app.dart
+++ lib/app.dart
@@ -1,19 +1,29 @@
+import 'package:animated_counter_app/Splash/Repository/splash_stream.dart';
 import 'package:flutter/material.dart';
-import 'Routes/routes.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Counter App',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData.light(),
-      darkTheme: ThemeData.dark(),
-      themeMode: ThemeMode.light,
-      onGenerateRoute: Routes.onGeneratedRoutes,
-      initialRoute: Routes.home,
+    return RepositoryProvider(
+      create: (_) => RoutesRepository(),
+      child: const AppView(),
     );
+  }
+}
+
+class AppView extends StatefulWidget {
+  const AppView({super.key});
+
+  @override
+  State<AppView> createState() => _AppViewState();
+}
+
+class _AppViewState extends State<AppView> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
   }
 }
