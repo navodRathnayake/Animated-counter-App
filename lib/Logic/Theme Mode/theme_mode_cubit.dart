c81ee3ca0df5f18ce95a8c5a45a78d46b27bef4f
Navod Rathnayake <82445185+navodRathnayake@users.noreply.github.com>
2023-02-25 13:12:20 +0530

    added hydrated

--- lib/Logic/Theme Mode/theme_mode_cubit.dart
+++ lib/Logic/Theme Mode/theme_mode_cubit.dart
@@ -1,10 +1,11 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flutter/material.dart';
+import 'package:hydrated_bloc/hydrated_bloc.dart';
 
 part 'theme_mode_state.dart';
 
-class ThemeModeCubit extends Cubit<ThemeModeState> {
+class ThemeModeCubit extends HydratedCubit<ThemeModeState> {
   ThemeModeCubit() : super(ThemeModeState.lightMode());
 
   void toggleSwitchChanged(bool isOn) {
@@ -17,5 +18,15 @@ class ThemeModeCubit extends Cubit<ThemeModeState> {
         emit(ThemeModeState.darkMode());
         debugPrint('dark mode has activated');
     }
+  }
+
+  @override
+  ThemeModeState? fromJson(Map<String, dynamic> json) {
+    return json['value'] as ThemeModeState;
+  }
+
+  @override
+  Map<String, dynamic>? toJson(ThemeModeState state) {
+    return {'status': state};
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,16 @@
 import 'package:animated_counter_app/Splash/Repository/splash_stream.dart';
 import 'package:animated_counter_app/app.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:hydrated_bloc/hydrated_bloc.dart';
+import 'package:path_provider/path_provider.dart';
 
-void main(List<String> args) {
+Future<void> main(List<String> args) async {
   WidgetsFlutterBinding.ensureInitialized();
+  HydratedBloc.storage = await HydratedStorage.build(
+    storageDirectory: kIsWeb
+        ? HydratedStorage.webStorageDirectory
+        : await getApplicationDocumentsDirectory(),
+  );
   runApp(App(repository: RoutesRepository()));
 }
